c8490b3938dbf6873ec41c6fd43c46b2a68a69a8
Amirkhon0707 <158686103+Amirkhon0707@users.noreply.github.com>
2024-02-26 16:06:13 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+var first, second, third, ans;
+
+first = +prompt('Введите первое число');
+second = +prompt('Введите второе число');
+third = +prompt('Введите третье число');
+
+ans = ((first + second + third) / 3);
+
+alert(ans)
